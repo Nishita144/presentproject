4a85ab203a85a938f9000f83b6068cdd770bbf0e
Nishita <challanishita07@gmail.com>
2022-11-27 22:10:16 +0530

    demo maven project

---
+++ src/main/java/pom/kmitmaven/presentproject/App.java
@@ -0,0 +1,13 @@
+package pom.kmitmaven.presentproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
